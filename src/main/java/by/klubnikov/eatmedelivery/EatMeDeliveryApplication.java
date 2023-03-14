3eaadb7a9816230e949e8e4e2f01692084853a45
Alexander Klubnikov <clubman066@gmail.com>
2023-03-14 23:42:21 +0300

    First commit.

---
+++ src/main/java/by/klubnikov/eatmedelivery/EatMeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package by.klubnikov.eatmedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatMeDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatMeDeliveryApplication.class, args);
+	}
+
+}
