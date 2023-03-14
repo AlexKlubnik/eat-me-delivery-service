f97b4779932570fe9ed8b9bfe7ed1463d512ae9f
Alexander Klubnikov <clubman066@gmail.com>
2023-03-14 23:42:21 +0300

    First commit.

---
+++ src/test/java/by/klubnikov/eatmedelivery/EatMeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package by.klubnikov.eatmedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatMeDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
